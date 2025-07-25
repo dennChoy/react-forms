27e2f5a1ea677c31e84afbc5a83bf6f89d1d40a2
Dennys <dennnyschoy@gmail.com>
2025-07-25 14:55:58 -0600

    Validations for state option added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,67 @@
+import React, { useState } from 'react';
+
 export default function Login() {
+
+  const [enteredValues, setEnteredValues] = useState({
+    email: '',
+    password: ''
+  });
+
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false
+  })
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes('@');
+
+  function handleSubmit(event){
+    event.preventDefault()
+  }
+
+  function handleInputBlur(identifier){
+    setDidEdit(prevEdit => ({
+      ...prevEdit,
+      [identifier]: true
+    }));
+  }
+
+  function handleInputChange(identifier, value){
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [identifier]:value
+    }));
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: false
+    }));
+  }
+
+  console.log(didEdit.email, " - ", enteredValues.email, " - ", emailIsInvalid);
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input 
+            id="email" 
+            type="email"
+            name="email"
+            onBlur={() => {handleInputBlur('email')}}
+            onChange={(event) => handleInputChange('email', event.target.value)}
+            value={enteredValues.email}
+          />
+          <div className='control-error'>{emailIsInvalid && <p>Please enter a valid email</p>}</div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input 
+            id="password" 
+            type="password" 
+            name="password" 
+          />
         </div>
       </div>
 
